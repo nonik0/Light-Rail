37f5b16c25087d25508bbd6985293a27dbdcb3cd
Nick Brown <nworbn@gmail.com>
2024-12-13 21:31:39 -0800

    starting boilerplate

--- firmware/game.h
+++ firmware/game.h
@@ -1,14 +1,19 @@
 #pragma once
 
 #include <Arduino.h>
+#include <Adafruit_IS31FL3731.h>
 
+#include "common.h"
 #include "platform.h"
 #include "train.h"
 #include "track.h"
 
+#define TRACK_COUNT 144
+
 // possible game modes (maybe consider inheritance from Game)
 enum GameMode
 {
+  ANIMATION,
   FREEPLAY,
   RACE,
   SURVIVAL,
@@ -17,11 +22,48 @@ enum GameMode
 
 class Game
 {
-public:
-  Game() {};
-
 private:
+  Adafruit_IS31FL3731 _trackLeds = Adafruit_IS31FL3731(); // TODO: write own optimized code for IS31
+
   int _mode;
   int _score;
   int _lives;
+
+public:
+  Game() {};
+
+  void setup();
+  void tick();
+  bool isOver();
+  void restart();
 };
+
+void Game::setup()
+{
+  _mode = ANIMATION;
+  _score = 0;
+
+  if (!_trackLeds.begin())
+  {
+    log("IS31 not found");
+    while (1)
+      ;
+  }
+  log("IS31 found!");
+}
+
+void Game::tick()
+{
+  // update game state
+  // update display
+}
+
+bool Game::isOver()
+{
+  return false;
+}
+
+void Game::restart()
+{
+  // reset game state
+}
--- firmware/track.h
+++ firmware/track.h
@@ -2,11 +2,22 @@
 
 #include <Arduino.h>
 
-class Track {
-  public:
-    Track() {};
-  private:
-    int _ledIndex;
-    Track* _next;
-    Track* _prev;
+
+struct Track {
+  uint8_t aConn; // neighbor on anode side
+  uint8_t cCOnn; // neighbor on cathode side
+  uint8_t aConn2; // neighbor2 on anode side
+  uint8_t cConn2; // neighbor2 on cathode side
 };
+
+uint32_t TrackData[144] = {
+  0x3538FFFF, // track 38 / 0x26
+  0x36260000, // track 53 / 0x35
+  0x35370000, // track 54 / 0x36
+  0x3F36002E, // track 55 / 0x37
+};
+
+
+
+// Train Data:
+// 1 byte per car?

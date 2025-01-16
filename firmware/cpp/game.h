aaf25e067c124f44fec0ad2cb9f9aca7ce723cee
Nick Brown <nworbn@gmail.com>
2025-01-15 19:23:12 -0800

    partial tone impl in rust

--- firmware/cpp/game.h
+++ firmware/cpp/game.h
@@ -9,6 +9,7 @@
 #include "platform.h"
 #include "train.h"
 #include "track.h"
+#include "tone.h"
 
 #define BUZZ 8 // PB4
 #define SW1 PB6
@@ -23,104 +24,6 @@
 #define SW10 PF1
 #define SW11 PF0
 #define SW12 PE6
-
-#define AVAILABLE_TONE_PINS 1
-#define USE_TIMER3
-
-const uint8_t PROGMEM tone_pin_to_timer_PGM[] = {3 /*, 1 */};
-static uint8_t tone_pins[AVAILABLE_TONE_PINS] = {255 /*, 255 */};
-
-volatile long timer3_toggle_count;
-volatile uint8_t *timer3_pin_port;
-volatile uint8_t timer3_pin_mask;
-
-// XXX: this function only works properly for timer 2 (the only one we use
-// currently).  for the others, it should end the tone, but won't restore
-// proper PWM functionality for the timer.
-void disableTimer(uint8_t _timer)
-{
-      bitWrite(TIMSK3, OCIE3A, 0);
-}
-
-
-void noTone(uint8_t _pin)
-{
-  int8_t _timer = -1;
-  
-  for (int i = 0; i < AVAILABLE_TONE_PINS; i++) {
-    if (tone_pins[i] == _pin) {
-      _timer = pgm_read_byte(tone_pin_to_timer_PGM + i);
-      tone_pins[i] = 255;
-      break;
-    }
-  }
-  
-  disableTimer(_timer);
-
-  digitalWrite(_pin, 0);
-}
-
-
-ISR(TIMER3_COMPA_vect)
-{
-  if (timer3_toggle_count != 0)
-  {
-    // toggle the pin
-    *timer3_pin_port ^= timer3_pin_mask;
-
-    if (timer3_toggle_count > 0)
-      timer3_toggle_count--;
-  }
-  else
-  {
-    disableTimer(3);
-    *timer3_pin_port &= ~(timer3_pin_mask); // keep pin low after stop
-  }
-}
-
-void tone(uint8_t _pin, unsigned int frequency, unsigned long duration)
-{
-  uint8_t prescalarbits = 0b001;
-  long toggle_count = 0;
-  uint32_t ocr = 0;
-  int8_t _timer = 3;
-
-  TCCR3A = 0;
-  TCCR3B = 0;
-  bitWrite(TCCR3B, WGM32, 1);
-  bitWrite(TCCR3B, CS30, 1);
-  timer3_pin_port = portOutputRegister(digitalPinToPort(_pin));
-  timer3_pin_mask = digitalPinToBitMask(_pin);
-
-  // Set the pinMode as OUTPUT
-  pinMode(_pin, OUTPUT);
-
-  // two choices for the 16 bit timers: ck/1 or ck/64
-  ocr = F_CPU / frequency / 2 - 1;
-
-  prescalarbits = 0b001;
-  if (ocr > 0xffff)
-  {
-    ocr = F_CPU / frequency / 2 / 64 - 1;
-    prescalarbits = 0b011;
-  }
-
-  TCCR3B = (TCCR3B & 0b11111000) | prescalarbits;
-
-  // Calculate the toggle count
-  if (duration > 0)
-  {
-    toggle_count = 2 * frequency * duration / 1000;
-  }
-  else
-  {
-    toggle_count = -1;
-  }
-
-  OCR3A = ocr;
-  timer3_toggle_count = toggle_count;
-  bitWrite(TIMSK3, OCIE3A, 1);
-}
 
 // TODO: make sure all game resources are statically allocated
 
@@ -310,41 +213,41 @@ void Game::readSwitches()
   else if (!(PIND & (1 << SW5)))
   {
     _boardDigits.display(5);
-    tone(BUZZ, 5000, 100);
+    tone2(BUZZ, 5000, 100);
   }
   else if (!(PINE & (1 << SW6)))
   {
     _boardDigits.display(6);
-    tone(BUZZ, 6000, 100);
+    tone2(BUZZ, 6000, 100);
   }
   else if (!(PIND & (1 << SW7)))
   {
     _boardDigits.display(7);
-    tone(BUZZ, 7000, 100);
+    tone2(BUZZ, 7000, 100);
   }
   else if (!(PIND & (1 << SW8)))
   {
     _boardDigits.display(8);
-    tone(BUZZ, 8000, 100);
+    tone2(BUZZ, 8000, 100);
   }
   else if (!(PINF & (1 << SW9)))
   {
     _boardDigits.display(9);
-    tone(BUZZ, 9000, 100);
+    tone2(BUZZ, 9000, 100);
   }
   else if (!(PINF & (1 << SW10)))
   {
     _boardDigits.display(10);
-    tone(BUZZ, 10000, 100);
+    tone2(BUZZ, 10000, 100);
   }
   else if (!(PINF & (1 << SW11)))
   {
     _boardDigits.display(11);
-    tone(BUZZ, 11000, 100);
+    tone2(BUZZ, 11000, 100);
   }
   else if (!(PINE & (1 << SW12)))
   {
     _boardDigits.display(12);
-    tone(BUZZ, 12000, 100);
+    tone2(BUZZ, 12000, 100);
   }
 }

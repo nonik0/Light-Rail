5b48181ca70dc7a161b093f454c1ecee85243237
Nick Brown <nworbn@gmail.com>
2025-01-30 18:12:24 -0800

    game logic progress

--- firmware/rust/src/game.rs
+++ firmware/rust/src/game.rs
@@ -1,4 +1,14 @@
-use crate::train;
+use atmega_hal::port::{
+    mode::{Input, PullUp},
+    Dynamic, Pin,
+};
+//use embedded_hal::digital::InputPin;
+use crate::as1115::AS1115;
+use is31fl3731::IS31FL3731;
+use crate::tone::Timer3Tone;
+
+use crate::train::Train;
+
 
 #[derive(Copy, Clone)]
 enum GameMode {
@@ -9,33 +19,41 @@ enum GameMode {
     Puzzle,
 }
 
-struct Game {
-    //track_graph: &'static Track,
-    // board_leds: AdafruitIS31FL3731,
-    // board_digits: AS1115,
+const MAX_TRAINS: usize = 5;
+const NUM_BUTTONS: usize = 12;
+const NUM_DIGITS: usize = 3;
+const DIGIT_INTENSITY: u8 = 3;
+
+struct Game<I2C> {
+    // board components
+    board_buttons: [Pin<Input<PullUp>, Dynamic>; NUM_BUTTONS],
+    board_buzzer: Timer3Tone,
+    board_digits: AS1115<I2C>,
+    board_leds: IS31FL3731<I2C>,
+
+    // game state
     mode: GameMode,
     is_over: bool,
-
-    num_trains: u8,
-    trains: [train::Train; Game::MAX_TRAINS],
+    trains: heapless::Vec<Train, MAX_TRAINS>,
     //platforms: [Platform; PLATFORM_COUNT],
 }
 
-impl Game {
-    const MAX_TRAINS: usize = 5;
-    const NUM_DIGITS: usize = 3;
-    const DIGIT_INTENSITY: u8 = 3;
-
-    // fn new() -> Self {
-    //     Game {
-    //         track_graph: unsafe { &*(TRACK_DATA as *const Track) },
-    //         board_leds: AdafruitIS31FL3731::new(),
-    //         board_digits: AS1115::new(0x00),
-    //         mode: GameMode::Animation,
-    //         is_over: false,
-    //         num_trains: 0,
-    //         trains: [Train::default(); Game::MAX_TRAINS],
-    //         platforms: [Platform::default(); PLATFORM_COUNT],
-    //     }
-    // }
+impl<I2C> Game<I2C> {
+    // do we need singleton enforcement with ownership?
+    pub fn new(
+        board_buttons: [Pin<Input<PullUp>, Dynamic>; NUM_BUTTONS],
+        board_buzzer: Timer3Tone,
+        board_digits: AS1115<I2C>,
+        board_leds: IS31FL3731<I2C>,
+    ) -> Self {
+        Self {
+            board_buttons,
+            board_buzzer,
+            board_digits,
+            board_leds,
+            mode: GameMode::Animation,
+            is_over: false,
+            trains: heapless::Vec::<Train, MAX_TRAINS>::new(),
+        }
+    }
 }
--- firmware/rust/src/train.rs
+++ firmware/rust/src/train.rs
@@ -1,21 +1,77 @@
-
+use crate::location::Location;
+use heapless::Vec;
 
 const MAX_CARS: usize = 5;
 const MIN_SPEED: u8 = 0;
 const MAX_SPEED: u8 = 100;
+const DEFAULT_LOCATION: u8 = 0xFF;
 const DEFAULT_SPEED: u8 = 10;
 const CAR_FULL_PWM: u8 = 200;
 const CAR_EMPTY_PWM: u8 = 50;
 
+#[derive(Debug)]
+pub enum Cargo {
+    Empty = 0,
+    Full = 1,
+}
+
+#[derive(Debug)]
 pub struct Car {
-    pub location: u8,
-    pub cargo: u8,
+    pub loc: Location,
+    pub cargo: Cargo,
+}
+
+pub struct Engine {
+    pub speed: u8,
+    pub counter: u8,
+}
+
+impl Default for Engine {
+    fn default() -> Self {
+        Self {
+            speed: DEFAULT_SPEED,
+            counter: 0,
+        }
+    }
 }
 
 pub struct Train {
-    pub engine_direction: bool,
-    pub cars: [Car; MAX_CARS],
-    pub num_cars: u8,
-    pub speed: u8,
-    pub speed_counter: u8,
+    pub engine: Engine,
+    pub cars: Vec<Car, MAX_CARS>,
+}
+
+impl Train {
+    
+    pub fn new(loc: Location, cargo: Cargo) -> Self {
+        let mut cars = Vec::new();
+        cars.push(Car { loc, cargo }).unwrap();
+        Self {
+            engine: Engine::default(),
+            cars,
+        }
+    }
+
+    pub fn advance(&mut self) -> bool {
+        self.engine.counter += self.engine.speed;
+
+        if self.engine.counter < MAX_SPEED {
+            return false;
+        }
+
+        self.engine.counter -= MAX_SPEED;
+
+        // move train from the rear, setting each LED accordingly
+        if !self.cars.is_empty() {
+            //set_led(self.cars[self.cars.len() - 1].loc, 0);
+            for i in (1..self.cars.len()).rev() {
+                self.cars[i].loc = self.cars[i - 1].loc;
+                //set_led(self.cars[i].loc, TODO);
+            }
+        }
+
+        // advance front car to next location, setting LED accordingly
+        //self.cars[0].loc = self.cars[0].loc.next();
+
+        true
+    }
 }

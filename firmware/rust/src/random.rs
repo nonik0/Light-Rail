5f6e2fad5435e87754eda3a87e3504faeb5eec94
Nick Brown <nworbn@gmail.com>
2025-02-10 16:12:59 -0800

    bug in graph data

--- firmware/rust/src/random.rs
+++ firmware/rust/src/random.rs
@@ -3,12 +3,17 @@
 #![allow(unused_variables)]
 
 /// A simple random number generator based on the Lehmer LCG algorithm. TODO: verify algorithm is correct
-
 use core::cell::Cell;
 use random_trait::Random;
 
-static RNG_STATE: avr_device::interrupt::Mutex<Cell<u32>> =
-    avr_device::interrupt::Mutex::new(Cell::new(0));
+static RNG_STATE: avr_device::interrupt::Mutex<Cell<RngState>> =
+    avr_device::interrupt::Mutex::new(Cell::new(RngState { value: 0, index: 0 }));
+
+#[derive(Clone, Copy, Default)]
+struct RngState {
+    value: u32,
+    index: usize,
+}
 
 // zero-sized type to represent the RNG
 #[derive(Clone, Copy, Default)]
@@ -18,7 +23,10 @@ impl Rng {
     pub fn seed(&mut self, seed: u32) {
         avr_device::interrupt::free(|cs| {
             let cell = RNG_STATE.borrow(cs);
-            cell.set(seed);
+            let mut state = cell.get();
+            state.value = seed;
+            state.value = state.value.wrapping_mul(1664525).wrapping_add(1013904223);
+            cell.set(state);
         });
     }
 }
@@ -27,24 +35,22 @@ impl Random for Rng {
     type Error = ();
     fn try_fill_bytes(&mut self, buf: &mut [u8]) -> Result<(), Self::Error> {
         avr_device::interrupt::free(|cs| {
-            let cell = RNG_STATE.borrow(cs);
-            let mut state = cell.get();
-            let mut rand_bytes = state.to_le_bytes();
-            let mut index = 0;
+            let state_cell = RNG_STATE.borrow(cs);
+            let mut state = state_cell.get();
+            let mut rand_bytes = state.value.to_le_bytes();
 
-            state = state.wrapping_mul(1664525).wrapping_add(1013904223);
             for e in buf.iter_mut() {
-                if index == 4 {
-                    state = state.wrapping_mul(1664525).wrapping_add(1013904223);
-                    rand_bytes = state.to_le_bytes();
-                    index = 0;
+                if state.index == 4 {
+                    state.value = state.value.wrapping_mul(1664525).wrapping_add(1013904223);
+                    rand_bytes = state.value.to_le_bytes();
+                    state.index = 0;
                 }
-                *e = rand_bytes[index];
-                index += 1;
+                *e = rand_bytes[state.index];
+                state.index += 1;
             }
 
-            cell.set(state);
-        }); 
+            state_cell.set(state);
+        });
 
         Ok(())
     }

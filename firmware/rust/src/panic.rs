0db5d1b99699be98aba25663c673e5efc44fa58c
Nick Brown <nworbn@gmail.com>
2025-03-31 12:41:10 -0700

    writeup draft, minor

--- firmware/rust/src/panic.rs
+++ firmware/rust/src/panic.rs
@@ -2,7 +2,7 @@
 use core::ptr::addr_of;
 use embedded_hal::delay::DelayNs;
 
-use crate::{Delay, I2c, DIGITS_COUNT, DIGITS_I2C_ADDR, DIGITS_INTENSITY};
+use crate::{Delay, I2c, NUM_DIGITS, DIGITS_I2C_ADDR, DIGITS_INTENSITY};
 
 #[macro_export]
 macro_rules! panic_to_digits {
@@ -42,18 +42,18 @@ fn panic(_: &core::panic::PanicInfo) -> ! {
         400_000,
     );
     let mut board_digits = as1115::AS1115::new(i2c, DIGITS_I2C_ADDR);
-    board_digits.init(DIGITS_COUNT, DIGITS_INTENSITY).unwrap();
+    board_digits.init(NUM_DIGITS, DIGITS_INTENSITY).unwrap();
 
     let panic_msg_len = unsafe { PANIC_MSG.iter().position(|&x| x == 0).unwrap_or(PANIC_MSG_MAX_LEN) };
-    let mut padded_msg = [b' '; PANIC_MSG_MAX_LEN + DIGITS_COUNT as usize];
+    let mut padded_msg = [b' '; PANIC_MSG_MAX_LEN + NUM_DIGITS as usize];
     let panic_msg_slice = unsafe { core::slice::from_raw_parts(addr_of!(PANIC_MSG) as *const u8, panic_msg_len) };
-    padded_msg[DIGITS_COUNT as usize..DIGITS_COUNT as usize + panic_msg_len].copy_from_slice(panic_msg_slice);
+    padded_msg[NUM_DIGITS as usize..NUM_DIGITS as usize + panic_msg_len].copy_from_slice(panic_msg_slice);
 
     let mut offset: usize = 0;
     loop {
-        let display_slice = &padded_msg[offset..offset + DIGITS_COUNT as usize];
+        let display_slice = &padded_msg[offset..offset + NUM_DIGITS as usize];
         board_digits.display_ascii(display_slice).unwrap();
-        offset = (offset + 1) % (panic_msg_len + DIGITS_COUNT as usize);
+        offset = (offset + 1) % (panic_msg_len + NUM_DIGITS as usize);
         delay.delay_ms(300);
     }
 }

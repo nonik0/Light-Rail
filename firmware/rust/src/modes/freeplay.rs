011f2e2dcdc35bd75019e1d91355cdfaf12470d9
Nick Brown <nick@altonimb.us>
2025-05-20 16:13:10 -0700

    basic working modes

--- firmware/rust/src/modes/freeplay.rs
+++ firmware/rust/src/modes/freeplay.rs
@@ -1,9 +1,17 @@
 use embedded_hal::i2c::I2c;
+use random_trait::Random;
 
-use crate::{game::GameState, input::InputEvent, modes::GameModeHandler};
+use crate::{common::*, game::{DisplayState, GameState}, input::InputEvent, modes::GameModeHandler, random::Rand};
 
-#[derive(Default)]
-pub struct FreeplayMode;
+pub struct FreeplayMode {
+    score: u16,
+}
+
+impl Default for FreeplayMode {
+    fn default() -> Self {
+        FreeplayMode { score: 0 }
+    }
+}
 
 impl GameModeHandler for FreeplayMode
 {
@@ -15,8 +23,31 @@ impl GameModeHandler for FreeplayMode
         1
     }
 
+    fn on_game_tick(&mut self, state: &mut GameState) {
+        for platform in state.platforms.iter_mut() {
+            if platform.is_empty() && Rand::default().get_u16() <= 50 {
+                platform.set_cargo();
+            }
+        }
+    }
+
     fn on_input_event(&mut self, event: InputEvent, state: &mut GameState) {
-        // Implement freeplay behavior here
-        // create/destroy trains, add cars, etc.
+        // TODO: add/remove trains, etc.
+    }
+
+    fn on_train_event(&mut self, train_index: usize, state: &mut GameState) {
+        let train = &state.trains[train_index];
+        let caboose_loc = train.caboose();
+        let last_loc = train.last_loc();
+
+        // Clear cargo if train front is at a platform with cargo
+        for platform in state.platforms.iter_mut() {
+            if !platform.is_empty() && train.engine() == platform.track_location() {
+                platform.clear_cargo();
+
+                self.score += 1;
+                state.display = DisplayState::Score(self.score);
+            }
+        }
     }
 }

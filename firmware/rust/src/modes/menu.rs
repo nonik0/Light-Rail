a0466a39e931e8eaa5b166854f1c82a1ccda7e26
Nick Brown <nick@altonimb.us>
2025-06-14 22:01:55 -0700

    anim fixes, incomplete

--- firmware/rust/src/modes/menu.rs
+++ firmware/rust/src/modes/menu.rs
@@ -16,7 +16,7 @@ use super::NUM_MODES;
 use as1115::segments::*;
 
 const SNAKE_LENGTH: usize = 5;
-const SNAKE_PERIOD: u8 = 30;
+const SNAKE_PERIOD: u8 = 100;
 
 #[derive(Clone, Copy, Debug, Default)]
 struct SnakeLocation {
@@ -240,6 +240,9 @@ impl MenuMode {
         // Helper to check if both locations are occupied
         let both_occupied =
             |a: SnakeLocation, b: SnakeLocation| self.is_occupied(&a) && self.is_occupied(&b);
+        let any_occupied = |a: SnakeLocation, b: SnakeLocation, c: SnakeLocation| {
+            self.is_occupied(&a) || self.is_occupied(&b) || self.is_occupied(&c)
+        };
 
         match (from.segment, direction, to.segment) {
             // G segment: can't move right through itself
@@ -264,44 +267,61 @@ impl MenuMode {
             }
             // Down from B: can't move down through itself
             (B, false, C) if from.digit < NUM_DIGITS - 1 && from.digit == to.digit => {
-                !both_occupied(
-                    SnakeLocation::new(from.digit, G),
-                    SnakeLocation::new(from.digit + 1, G),
-                )
+                self.is_occupied(&SnakeLocation::new(from.digit, G))
+                    && any_occupied(
+                        SnakeLocation::new(from.digit + 1, F),
+                        SnakeLocation::new(from.digit + 1, G),
+                        SnakeLocation::new(from.digit + 1, E),
+                    )
             }
             // Down from F: can't move down through itself
-            (F, false, E) if from.digit > 0 && from.digit == to.digit => !both_occupied(
-                SnakeLocation::new(from.digit, G),
-                SnakeLocation::new(from.digit - 1, G),
-            ),
+            (F, false, E) if from.digit > 0 && from.digit == to.digit => {
+                self.is_occupied(&SnakeLocation::new(from.digit, G))
+                    && any_occupied(
+                        SnakeLocation::new(from.digit - 1, B),
+                        SnakeLocation::new(from.digit - 1, G),
+                        SnakeLocation::new(from.digit - 1, C),
+                    )
+            }
             // Up from C: can't move up through itself
             (C, true, B) if from.digit < NUM_DIGITS - 1 && from.digit == to.digit => {
-                !both_occupied(
-                    SnakeLocation::new(from.digit, G),
-                    SnakeLocation::new(from.digit + 1, G),
-                )
+                self.is_occupied(&SnakeLocation::new(from.digit, G))
+                    && any_occupied(
+                        SnakeLocation::new(from.digit + 1, F),
+                        SnakeLocation::new(from.digit + 1, G),
+                        SnakeLocation::new(from.digit + 1, E),
+                    )
             }
             // Up from E: can't move up through itself
-            (E, true, F) if from.digit > 0 && from.digit == to.digit => !both_occupied(
-                SnakeLocation::new(from.digit, G),
-                SnakeLocation::new(from.digit - 1, G),
-            ),
+            (E, true, F) if from.digit > 0 && from.digit == to.digit => {
+                self.is_occupied(&SnakeLocation::new(from.digit, G))
+                    && any_occupied(
+                        SnakeLocation::new(from.digit - 1, B),
+                        SnakeLocation::new(from.digit - 1, G),
+                        SnakeLocation::new(from.digit - 1, C),
+                    )
+            }
             _ => true,
         }
     }
 
     // Finds a path from loc of length path_left, and returns the first location and new direction in that path.
-    fn find_path(&self, loc: &SnakeLocation, path_left: u8) -> Option<(SnakeLocation, bool)> {
+    fn find_path(
+        &self,
+        loc: &SnakeLocation,
+        dir: bool,
+        path_left: u8,
+    ) -> Option<(SnakeLocation, bool)> {
         if path_left == 0 {
             return Some((loc.clone(), false));
         }
 
-        let mut next_locations = self.candidate_snake_locations(loc, self.snake_direction);
+        let mut next_locations = self.candidate_snake_locations(loc, dir);
         Self::shuffle(&mut next_locations);
-        for (next_loc, dir) in next_locations.iter() {
-            if self.is_valid_movement(loc, *dir, next_loc) {
-                if let Some(_) = self.find_path(next_loc, path_left - 1) {
-                    return Some((next_loc.clone(), *dir));
+        for (next_loc, next_dir) in next_locations.iter() {
+            if self.is_valid_movement(loc, dir, next_loc) {
+                if let Some(_) = self.find_path(next_loc, *next_dir, path_left - 1) {
+                    return Some((next_loc.clone(), next_dir));
                 }
             }
         }
@@ -316,18 +336,24 @@ impl MenuMode {
         let mut new_direction = self.snake_direction;
 
         // search for a path of length 3, 2, or 1
-        for path_length in 3..=1 {
-            if let Some((next_loc, dir)) = self.find_path(snake_head, path_length) {
+        for path_length in (1..=3).rev() {
+            if let Some((next_loc, next_dir)) =
+                self.find_path(snake_head, self.snake_direction, path_length)
+            {
                 new_head = Some(next_loc);
-                new_direction = dir;
+                new_direction = next_dir;
                 break;
             }
         }
 
         // if still no path found, just move to random candidate location
         if new_head.is_none() {
-            let next_locs = self.candidate_snake_locations(self.snake_segments.front().unwrap(), self.snake_direction);
-            let (next_loc, next_direction) = next_locs[Rand::default().get_u8() as usize % next_locs.len()];
+            let next_locs = self.candidate_snake_locations(
+                self.snake_segments.front().unwrap(),
+                self.snake_direction,
+            );
+            let (next_loc, next_direction) =
+                next_locs[Rand::default().get_u8() as usize % next_locs.len()];
             new_head = Some(next_loc);
             new_direction = next_direction;
         }
@@ -336,7 +362,9 @@ impl MenuMode {
         let new_head = new_head.unwrap();
         self.snake_segments.pop_back().unwrap();
         self.snake_direction = new_direction;
-        self.snake_segments.push_front(SnakeLocation::new(new_head.digit, new_head.segment)).unwrap();
+        self.snake_segments
+            .push_front(SnakeLocation::new(new_head.digit, new_head.segment))
+            .unwrap();
     }
 
     fn snake_segment_data(&self) -> [u8; NUM_DIGITS as usize] {

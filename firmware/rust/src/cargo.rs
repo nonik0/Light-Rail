5f25618851ba2b4bca84801e9653ccb44f211dcb
Nick Brown <nick@altonimb.us>
2025-06-16 23:20:54 -0700

    slow down patterns

--- firmware/rust/src/cargo.rs
+++ firmware/rust/src/cargo.rs
@@ -39,23 +39,23 @@ impl LedPattern {
     pub fn get_pwm(&self, phase: u8, min_b: u8, max_b: u8) -> u8 {
         match self {
             LedPattern::Solid => max_b,
-            LedPattern::Blink1 => match phase % 64 {
-                0..=11 => min_b, // 12 ticks off
-                _ => max_b,      // 52 ticks on
+            LedPattern::Blink1 => match phase % 128 {
+                0..=23 => min_b, // 24 ticks off
+                _ => max_b,
             },
-            LedPattern::Blink2 => match phase % 64 {
-                0..=7 => min_b,   // 8 ticks off
-                8..=15 => max_b,  // 8 ticks on
-                16..=23 => min_b, // 8 ticks off
-                _ => max_b,       // 40 ticks on
+            LedPattern::Blink2 => match phase % 128 {
+                0..=15 => min_b,   // 16 ticks off
+                16..=31 => max_b,  // 16 ticks on
+                32..=47 => min_b, // 16 ticks off
+                _ => max_b,
             },
-            LedPattern::Blink3 => match phase % 64 {
-                0..=5 => min_b,   // 6 ticks off
-                6..=11 => max_b,  // 6 ticks on
-                12..=17 => min_b, // 6 ticks off
-                18..=23 => max_b, // 6 ticks on
-                24..=29 => min_b, // 6 ticks off
-                _ => max_b,       // 34 ticks on
+            LedPattern::Blink3 => match phase % 128 {
+                0..=11 => min_b,   // 12 ticks off
+                12..=23 => max_b,  // 12 ticks on
+                24..=35 => min_b, // 12 ticks off
+                36..=47 => max_b, // 12 ticks on
+                48..=59 => min_b, // 12 ticks off
+                _ => max_b,
             },
             LedPattern::Fade => {
                 // Fade up for phase 0..127, fade down for 128..255

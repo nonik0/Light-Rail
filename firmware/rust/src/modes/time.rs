84337a0ad97b62c8a247fe6f1fd56284a63ad20b
Nick Brown <nick@altonimb.us>
2025-06-16 18:00:16 -0700

    use decimals for timer indicator

--- firmware/rust/src/modes/time.rs
+++ firmware/rust/src/modes/time.rs
@@ -8,6 +8,7 @@ use crate::{
     location::NUM_PLATFORMS,
     modes::GameModeHandler,
     random::Rand,
+    NUM_DIGITS,
 };
 
 pub struct CargoTimer {
@@ -18,13 +19,14 @@ pub struct CargoTimer {
 pub struct TimeMode {
     score: u16,
     timers: Vec<CargoTimer, { TimeMode::MAX_CARGO as usize }>,
+    timer_dots: u8, // indicate time left with the 3 decimal points on display
 }
 
 impl TimeMode {
     const MAX_CARGO: u8 = 3;
     const MAX_SPEED: u8 = 15;
     const SPEED_INC: u8 = 5;
-    const TIMER_TICKS: u16 = 8000;
+    const TIMER_TICKS: u16 = 8000; // ~ 120 seconds with current runtime at 10ms base delay
 
     fn add_platform_timer(&mut self, platform_index: u8) {
         if self.timers.is_full()
@@ -54,6 +56,19 @@ impl TimeMode {
             crate::panic_with_error!(400);
         }
     }
+
+    fn get_display(&self) -> DisplayState {
+        let mut segment_data = [0u8; NUM_DIGITS as usize];
+        segment_data[0] = as1115::NUMBERS[((self.score / 100) % 10) as usize];
+        segment_data[1] = as1115::NUMBERS[((self.score / 10) % 10) as usize];
+        segment_data[2] = as1115::NUMBERS[(self.score % 10) as usize];
+
+        for i in (3 - self.timer_dots as usize)..3 {
+            segment_data[i] |= as1115::segments::DP;
+        }
+
+        DisplayState::Segments(segment_data)
+    }
 }
 
 impl Default for TimeMode {
@@ -61,6 +76,7 @@ impl Default for TimeMode {
         TimeMode {
             score: 0,
             timers: Vec::new(),
+            timer_dots: NUM_DIGITS,
         }
     }
 }
@@ -68,29 +84,48 @@ impl Default for TimeMode {
 impl GameModeHandler for TimeMode {
     fn on_restart(&mut self, state: &mut GameState) {
         self.score = 0;
+        self.timer_dots = NUM_DIGITS;
         self.timers.clear();
 
         state.is_over = false;
-        state.display = DisplayState::Score(self.score);
         state.init_trains(Cargo::Empty, 3, 5);
         state.clear_platforms();
+        state.display = self.get_display();
     }
 
     fn on_game_tick(&mut self, state: &mut GameState) {
+        let mut timer_update = false;
         for timer in self.timers.iter_mut() {
             timer.ticks_left = timer.ticks_left.saturating_sub(1);
 
             let timer_platform = &mut state.platforms[timer.platform_index as usize];
-            match timer.ticks_left {
-                3000 => timer_platform.set_phase_speed(2),
-                1000 => timer_platform.set_phase_speed(3),
-                0 => {
-                    state.display = DisplayState::Text(*b"ovr");
-                    state.is_over = true;
-                    return;
+            if timer.ticks_left == (Self::TIMER_TICKS >> 1) {
+                timer_platform.set_phase_speed(2);
+                if self.timer_dots > 2 {
+                    self.timer_dots = 2;
+                    timer_update = true;
                 }
-                _ => {}
+            } else if timer.ticks_left == (Self::TIMER_TICKS >> 2) {
+                timer_platform.set_phase_speed(3);
+                if self.timer_dots > 1 {
+                    self.timer_dots = 1;
+                    timer_update = true;
+                }
+            } else if timer.ticks_left == (Self::TIMER_TICKS >> 3) {
+                timer_platform.set_phase_speed(6);
+                if self.timer_dots > 0 {
+                    self.timer_dots = 0;
+                    timer_update = true;
+                }
+            } else if timer.ticks_left == 0 {
+                state.display = DisplayState::Text(*b"ovr");
+                state.is_over = true;
+                return;
             }
+        }
+
+        if timer_update {
+            state.display = self.get_display();
         }
 
         if !self.timers.is_full() {
@@ -125,7 +160,7 @@ impl GameModeHandler for TimeMode {
                                 platform.clear_cargo();
                                 self.remove_platform_timer(platform_index as u8);
                                 self.score += 1;
-                                state.display = DisplayState::Score(self.score);
+                                state.display = self.get_display();
 
                                 if self.score == 3 || self.score == 10 || self.score == 20 {
                                     train.add_car(Cargo::Empty);
